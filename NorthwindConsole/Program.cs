5c57e059172b53055688376b5da5eb657cc2c035
Katie Bratberg <57917884+kbratberg@users.noreply.github.com>
2020-12-03 11:29:16 -0600

    Added display specific product option

--- NorthwindConsole/Program.cs
+++ NorthwindConsole/Program.cs
@@ -26,19 +26,55 @@ namespace NorthwindConsole
                     Console.WriteLine("2) Add Category");
                     Console.WriteLine("3) Display Category and related products");
                     Console.WriteLine("4) Display all Categories");
-                    Console.WriteLine("5) Display Products");
-                    Console.WriteLine("6) Add Product");
-                    Console.WriteLine("7) Edit a Product");
-                    Console.WriteLine("8) Display all Products");
-                    Console.WriteLine("9) Display Individual Product");
+                    Console.WriteLine("5) Display All Categories and their Active Products");
+                    Console.WriteLine("6) Display Specific Product details");
+                    Console.WriteLine("7) Add Product");
+                    Console.WriteLine("8) Edit a Product");
+                    Console.WriteLine("9) Display all Products");
+                    Console.WriteLine("10) Display Individual Product");
                     Console.WriteLine("\"q\" to quit");
                     choice = Console.ReadLine();
                     Console.Clear();
 
 
                     logger.Info($"Option {choice} selected");
+                    if(choice == "1"){
+                        //edit Category
+                    }else if (choice == "2")
+                    {   //add Category
+                        var db = new NorthwindConsole_32_KMBContext();
+                        Category category = InputCategory(db);
+
+                        if (category != null)
+                        {
+                            db.Categories.Add(category);
+                            db.SaveChanges();
+                        }
+                    }else if (choice == "3")
+                    {   //display category and related products
+                        var db = new NorthwindConsole_32_KMBContext();
+                        var query = db.Categories.OrderBy(p => p.CategoryId);
+
+                        Console.WriteLine("Select the category whose products you want to display:");
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
+                        }
+                        Console.ForegroundColor = ConsoleColor.White;
+                        int id = int.Parse(Console.ReadLine());
+                        Console.Clear();
+                        logger.Info($"CategoryId {id} selected");
+                        Category category = db.Categories.Include("Products").FirstOrDefault(c => c.CategoryId == id);
+                        Console.WriteLine($"{category.CategoryName} - {category.Description}");
+                        foreach (Product p in category.Products)
+                        {
+                            Console.WriteLine(p.ProductName);
+                        }
+                    }
                     if (choice == "4")
                     {
+                        // display all categories
                         var db = new NorthwindConsole_32_KMBContext();
                         var query = db.Categories.OrderBy(p => p.CategoryName);
 
@@ -50,17 +86,74 @@ namespace NorthwindConsole
                             Console.WriteLine($"{item.CategoryName} - {item.Description}");
                         }
                         Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    if (choice == "8")
+                    }else if (choice == "5")
                     {
-                        
+                        //Display all categories and their related active products
+                        var db = new NorthwindConsole_32_KMBContext();
+                        var categoryQuery = db.Categories.Include("Products").OrderBy(p => p.CategoryName);
+                     
+                        foreach (var item in categoryQuery)
+                        {   
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"{item.CategoryName}");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            foreach (Product p in item.Products)
+                            {
+                                if(p.Discontinued == false){
+                                Console.WriteLine($"\t{p.ProductName}");
+                                }
+                            }
+                        }
+                    
+                    Console.WriteLine();
+                    }
+                    else if (choice == "6"){
+                        // do do display products
+                        var db = new NorthwindConsole_32_KMBContext();
+                        var query = db.Products.OrderBy(p => p.ProductName);
+                        var product = GetProduct(db);
+
+                        if(product != null){
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine($"ProductId: {product.ProductId}\nProduct Name: {product.ProductName}\nSupplierId: {product.SupplierId}\nCategoryId: {product.CategoryId}\nQuantity Per Unit: {product.QuantityPerUnit}");
+                        Console.WriteLine($"Unit Price: {product.UnitPrice}\nUnits in Stock: {product.UnitsInStock}\nUnits On Order: {product.UnitsOnOrder}\nReorder Level: {product.ReorderLevel}");
+                        if(product.Discontinued == true){
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Discontinued");
+                        }else if(product.Discontinued == false){
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Active");
+                        }
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+                     else if (choice == "7")
+                    {
+                        // add a product
+                        var db = new NorthwindConsole_32_KMBContext();
+                        Product product = InputProduct(db);
+
+                        if (product != null)
+                        {
+                            db.Products.Add(product);
+                            db.SaveChanges();
+                        }
+                    }
+                    else if (choice == "9")
+                    {
+                        // display products
+                        //pick an option to display
                         Console.WriteLine("Select Which Products to Display");
                         Console.WriteLine("1)Display all Products");
                         Console.WriteLine("2)Display Active Products");
                         Console.WriteLine("3)Display Discontinued Products");
 
                         var displayChoice = Console.ReadLine();
-                        logger.Info($"Ypur choice: {displayChoice}");
+                        logger.Info($"Your choice: {displayChoice}");
+                        
+                        // log error if invalid choice
+                    
                         if(displayChoice != "1" || displayChoice != "2" || displayChoice != "3" ){
                             logger.Info("Invalid Choice");
                         }
@@ -68,7 +161,7 @@ namespace NorthwindConsole
 
                         if (displayChoice == "1")
                         {
-
+                                // display all Products Name only
                             var query = db.Products.OrderBy(p => p.ProductName);
                             Console.ForegroundColor = ConsoleColor.Green;
                             Console.WriteLine($"{query.Count()} records returned");
@@ -116,54 +209,10 @@ namespace NorthwindConsole
 
                         }
                     }
-                    else if (choice == "2")
-                    {
-                        var db = new NorthwindConsole_32_KMBContext();
-                        Category category = InputCategory(db);
-
-                        if (category != null)
-                        {
-                            db.Categories.Add(category);
-                            db.SaveChanges();
-                        }
-                    }
-                    else if (choice == "3")
-                    {
-                        var db = new NorthwindConsole_32_KMBContext();
-                        var query = db.Categories.OrderBy(p => p.CategoryId);
-
-                        Console.WriteLine("Select the category whose products you want to display:");
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        foreach (var item in query)
-                        {
-                            Console.WriteLine($"{item.CategoryId}) {item.CategoryName}");
-                        }
-                        Console.ForegroundColor = ConsoleColor.White;
-                        int id = int.Parse(Console.ReadLine());
-                        Console.Clear();
-                        logger.Info($"CategoryId {id} selected");
-                        Category category = db.Categories.Include("Products").FirstOrDefault(c => c.CategoryId == id);
-                        Console.WriteLine($"{category.CategoryName} - {category.Description}");
-                        foreach (Product p in category.Products)
-                        {
-                            Console.WriteLine(p.ProductName);
-                        }
-                    }
-                    else if (choice == "4")
-                    {
-                        var db = new NorthwindConsole_32_KMBContext();
-                        var query = db.Categories.Include("Products").OrderBy(p => p.CategoryId);
-                        foreach (var item in query)
-                        {
-                            Console.WriteLine($"{item.CategoryName}");
-                            foreach (Product p in item.Products)
-                            {
-                                Console.WriteLine($"\t{p.ProductName}");
-                            }
-                        }
-                    }
-                    Console.WriteLine();
-
+                    
+                    
+                    
+                   
                 } while (choice != "q");
             }
             catch (Exception ex)
